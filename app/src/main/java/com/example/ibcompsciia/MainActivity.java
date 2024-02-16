524f04bd5b9fe82362057073971b508fa5c64157
Atharv Allepally <atharvabhi@protonmail.com>
2024-02-16 13:18:10 -0800

    added button actions

--- app/src/main/java/com/example/ibcompsciia/MainActivity.java
+++ app/src/main/java/com/example/ibcompsciia/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.ibcompsciia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -14,9 +16,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Toast toast = Toast.makeText(this /* MyActivity */, "Yayyy first code", Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(this /* MyActivity */, " Yayyy first code", Toast.LENGTH_SHORT);
         toast.show();
-        //Button button = (Button) findViewById(R.)
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                toast.show();
+            }
+        });
+
 
     }
 }
